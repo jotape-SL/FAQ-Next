f48131eb1769c80a44dc93a377a649e588538b0b
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-09-06 13:42:01 -0300

    home page card, link, adjustment

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import dados from '../dados.json';
 export default function HomeScreen() {
   const infos = {
     nome: 'Peter Parker',
-  }
+  };
   const posts = dados.posts;
 
   return (
@@ -25,59 +25,63 @@ export default function HomeScreen() {
           borderRadius: '50%',
           margin: '0 auto',
           border: '2px solid #0008fa',
-          objectFit:'cover'
+          objectFit: 'cover',
         }}
       />
       <Text
-        variant="heading1"
-        tag="h1"
+        variant='heading1'
+        tag='h1'
         styleSheet={{ color: '#f73434', justifyContent: 'center' }}
       >
         {infos.nome}
       </Text>
-      
-      <Box styleSheet={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-        marginTop: '16px',
-        gridGap: '16px',
-      }}>
+
+      <Box
+        styleSheet={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+          marginTop: '16px',
+          gridGap: '16px',
+        }}
+      >
         {posts.map(({ title, content, id }) => (
           <Post key={id} title={title} content={content} id={id} />
         ))}
       </Box>
     </Box>
-  )
+  );
 }
 
 function Post({ title, content, id }) {
   return (
-    <Box 
-      styleSheet={{
-        flexDirection: 'column',
-        border: '1px solid #0008fa',
-        padding: '16px',
-        boxShadow: '1px 1px 5px 0 rgba(255,69,0,0.2)',
-        transition: '.3s',
-        borderRadius: '4px',
-        hover: {
-          boxShadow: '1px 1px 5px 5px rgba(0,8,250,0.2)',
-          cursor:"pointer"
-        }
-      }}
-    >
-      <NextLink href={`posts/${id}`} passHref>
+    <NextLink href={`posts/${id}`} passHref>
+      <Box
+        styleSheet={{
+          flexDirection: 'column',
+          border: '1px solid #0008fa',
+          padding: '16px',
+          boxShadow: '1px 1px 5px 0 rgba(255,69,0,0.2)',
+          transition: '.3s',
+          borderRadius: '4px',
+          hover: {
+            boxShadow: '1px 1px 5px 5px rgba(0,8,250,0.2)',
+            cursor: 'pointer',
+          },
+        }}
+      >
         <Text
-          tag="a"
-          variant="heading4"
-          styleSheet={{ display:' block', color: '#f73434', marginBottom: '8px' }}
+          tag='a'
+          variant='heading4'
+          styleSheet={{
+            display: ' block',
+            color: '#f73434',
+            marginBottom: '8px',
+          }}
         >
           {title}
         </Text>
-      </NextLink>
-      <Text>
-        {content.substring(0, 140)}...
-      </Text>
-    </Box>
+        <Text>{content.substring(0, 140)}...</Text>
+      </Box>
+    </NextLink>
   );
 }
